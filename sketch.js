d83e768eaa69f2ed72cccf31fd8d822b3de23f9a
Akshith28 <63508329+Akshith28@users.noreply.github.com>
2020-05-13 11:27:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+var streams = [];
+var fadeInterval = 1.6;
+var symbolSize = 10;
+
+function setup() {
+  createCanvas(2000,1000);
+  
+  background(0);
+
+  var x = 0;
+  for (var i = 0; i <= width / symbolSize; i++) {
+    var stream = new Stream();
+    stream.generateSymbols(x, random(-2000, 0));
+    streams.push(stream);
+    x += symbolSize
+  }
+
+  textFont('Consolas');
+  textSize(symbolSize);
+}
+
+function draw() {
+  background(0, 150);
+  streams.forEach(function(stream) {
+    stream.render();
+  });
+}
+
+function Symbol(x, y, speed, first, opacity) {
+  this.x = x;
+  this.y = y;
+  this.value;
+
+  this.speed = speed;
+  this.first = first;
+  this.opacity = opacity;
+
+  this.switchInterval = (random(2, 25));
+
+  this.setToRandomSymbol = function() {
+    var charType = (random(0, 5));
+    if (frameCount % 60 === 0) {
+      if (charType > 1) {
+        this.value = String.fromCharCode(
+          0x30A0 + floor(random(0, 97))
+        );
+      } else {
+        this.value = floor(random(0,10));
+      }
+    }
+  }
+
+  this.rain = function() {
+    this.y = (this.y >= height) ? 0 : this.y += this.speed;
+  }
+
+}
+
+function Stream() {
+  this.symbols = [];
+  this.totalSymbols =(random(5, 35));
+  this.speed = random(5, 22);
+
+  this.generateSymbols = function(x, y) {
+    var opacity = 255;
+    var first = (random(0, 4)) == 1;
+    for (var i =0; i <= this.totalSymbols; i++) {
+      symbol = new Symbol(
+        x,
+        y,
+        this.speed,
+        first,
+        opacity
+      );
+      symbol.setToRandomSymbol();
+      this.symbols.push(symbol);
+      opacity -= (255 / this.totalSymbols) / fadeInterval;
+      y -= symbolSize;
+      first = false;
+    }
+  }
+
+  this.render = function() {
+    this.symbols.forEach(function(symbol) {
+      if (symbol.first) {
+        fill(140, 255, 170, symbol.opacity);
+      } else {
+        fill(0, 255, 70, symbol.opacity);
+      }
+      text(symbol.value, symbol.x, symbol.y);
+      symbol.rain();
+      symbol.setToRandomSymbol();
+    });
+  }
+}
+
